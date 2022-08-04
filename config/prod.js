71ae9a5d3882cd3dee3ddb4a8984a942de9e7816
dvir <dvir6g6@gmail.com>
2022-08-04 20:04:35 +0300

    initial heroku app

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   dbURL:
-    'mongodb+srv://dvirAvraham:Aa123456@cluster0.ncwat.mongodb.net/toy_db?retryWrites=true&w=majority',
+    'mongodb+srv://dvirAvraham:Aa123456@cluster0.ncwat.mongodb.net/ey-task_db?retryWrites=true&w=majority',
 };
